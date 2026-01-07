beff1cb52f63d0f2ef6004b471af2af37838680e
J2H <jh.jeon3233@gmail.com>
2026-01-08 00:46:50 +0900

    fix : Prisma 프록시 객체 타입 추론 개선 및 에러 수정
    - Reflect.get을 도입으로 프록시의 property에서 발생하는 타입 에러 해결
    - this 바인딩 유지를 위한 bind 추가
    
    Issue#10

--- src/config/prisma.config.ts
+++ src/config/prisma.config.ts
@@ -2,19 +2,23 @@ import { PrismaClient } from '@prisma/client';
 import { AsyncLocalStorage } from 'async_hooks';
 import type { Prisma } from '@prisma/client';
 
-type PrismaTransactionalClient = PrismaClient | Prisma.TransactionClient;
-
+// 기본 PrismaClient 인스턴스 생성
 const prismaClient = new PrismaClient();
 
 // 트랜잭션 컨텍스트를 유지하기 위한 AsyncLocalStorage 저장소 생성
 export const transactionStorage = new AsyncLocalStorage<Prisma.TransactionClient>();
 
 // prismaClient를 감싸서 호출 시점에 트랜잭션 개체가 있으면 해당 개체를 사용하도록 프록시 생성
-export const prisma = new Proxy(prismaClient, { 
-  get(target, property) {                       
+export const prisma : PrismaClient = new Proxy(prismaClient, { 
+  get(target, property, receiver) {                       
     const tx = transactionStorage.getStore();   
     const client = tx || target;              
-    return (client as PrismaTransactionalClient)[property];           
+    const value = Reflect.get(client, property, receiver);
+
+    if (typeof value === 'function') {
+      return value.bind(client);
+    }
+    return value;
   }
 });
 
