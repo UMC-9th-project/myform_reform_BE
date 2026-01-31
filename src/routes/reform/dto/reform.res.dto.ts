dd79dddfbb56b9cf19f8841ae91ebc1f3219a0d5
seoki <seoki180@naver.com>
2026-02-01 02:57:09 +0900

    Chore : 주석제거
    Signed-off-by: seoki <seoki180@naver.com>

--- src/routes/reform/dto/reform.res.dto.ts
+++ src/routes/reform/dto/reform.res.dto.ts
@@ -1,49 +1,5 @@
 import { UUID } from '../../../@types/common.js';
 
-// type ProposalItem = Prisma.reform_proposalGetPayload<{
-//   select: {
-//     title: true;
-//     price: true;
-//     avg_star: true;
-//     review_count: true;
-//     reform_proposal_photo: {
-//       select: {
-//         content: true;
-//       };
-//     };
-//     owner: {
-//       select: { name: true };
-//     };
-//   };
-// }>;
-
-// type ReformRequest = Prisma.reform_requestGetPayload<{
-//   select: {
-//     title: true;
-//     min_budget: true;
-//     max_budget: true;
-//     reform_request_photo: {
-//       select: {
-//         content: true;
-//       };
-//     };
-//   };
-// }>;
-
-// type ReformRequestListResDto = Prisma.reform_requestGetPayload<{
-//   select: {
-//     min_budget: true;
-//     max_budget: true;
-//     title: true;
-//     reform_request_photo: {
-//       take: 1;
-//       select: {
-//         content: true;
-//       };
-//     };
-//   };
-// }>;
-
 export interface ReformHomeResponse {
   requests: ReformRequestResponseDto[];
   proposals: ReformProposalResponseDto[];
