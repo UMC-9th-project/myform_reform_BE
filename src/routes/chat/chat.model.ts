8b09681c5fea5cd82b65d5817f6f0d7a2097f4a8
J2H <jh.jeon3233@gmail.com>
2026-02-03 18:53:47 +0900

    feat: 채팅 이미지 처리 방식
    - 이미지 메세지 처리 추가
    - patch로직에서 삭제된 이미지 s3에서 삭제되도록 처리
    
    Issue#74

--- src/routes/chat/chat.model.ts
+++ src/routes/chat/chat.model.ts
@@ -1,4 +1,3 @@
-import { text } from 'express';
 import { InvalidChatRoomTypeError, InvalidChatMessageTypeError } from './chat.error.js';
 import { chat_message } from '@prisma/client';
 
@@ -8,14 +7,15 @@ export type MessageType = 'image' | 'request' | 'proposal' | 'text' | 'payment' 
 
 // 채팅방 payload에 담길 타입 정의
 export type ChatRoomPayload = 
-    | { id: string, title: string, price : number, image: string }
-    | { id: string, title: string, minBudget : number, maxBudget : number, image: string }
-    | { id: string };
+    | { id: string, title: string, price : number, image: string }                          // 제안서로 접근
+    | { id: string, title: string, minBudget : number, maxBudget : number, image: string }  // 요청서로 접근
+    | { id: string };                                                                        // 피드로 접근
 
 // 채팅메시지 payload에 담길 타입 정의
 export type ChatMessagePayload = 
     | {id: string, price: number, delivery: number, expected_working: Date }  //제안서
-    | {id: string, title: string, minBudget: number, maxBudget: number}              //요청서
+    | {id: string, title: string, minBudget: number, maxBudget: number}       //요청서
+    | {urls: string[]}                                                        //이미지
     | undefined;                                                              //텍스트
 
 // 채팅 메세지 생성 파라미터 인터페이스
@@ -119,7 +119,7 @@ export class ChatRoom {
 
 export class ChatMessageFactory {
 
-  private static readonly PAYLOAD_TYPES: MessageType[] = ['request', 'proposal', 'payment', 'result'];
+  private static readonly PAYLOAD_TYPES: MessageType[] = ['request', 'proposal', 'payment', 'result', 'image'];
 
   private static assemble(
     chatRoomId : string, 
@@ -184,7 +184,11 @@ export class ChatMessageFactory {
     };
   }
 
-
+  static mapToImagePayload(target : string[]): ChatMessagePayload {
+    return {
+      urls: target
+    };
+  }
 
   // 타입별 payload 변환로직 필요시 구현
   // private static mapToPayload(target: any, type: string): ChatMessagePayload {
